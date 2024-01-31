f26304cd6162a6e0f6ad95040a5db87982a7dff7
gurnoorjatana2 <124634894+gurnoorjatana2@users.noreply.github.com>
2024-01-30 23:19:18 -0700

    Added Week-2 Files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+export default function SudentInfo () {
+    return(
+        <main>
+            <p>Gurnoor Singh Jatana</p>
+            <ul>
+                <li>
+                    <Link href = "https://github.com/gurnoorjatana2?tab=repositories">My Github Link</Link>
+                </li>
+            </ul>
+        </main>
+
+    )
+}
